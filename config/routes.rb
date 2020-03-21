d157b348b4149f87e53177b36ed3250295aa101c
anthonyamar <anthonyamar@labodessons.fr>
2020-03-21 12:24:58 +0100

    devise params

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'static_pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
 end
